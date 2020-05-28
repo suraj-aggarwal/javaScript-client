a45aa83cfde5d20645b8e6ee3454595eb9a3c1f4
Suraj Aggarwal <suraj.aggarwal@lapr243.successivetech.com>
2020-05-28 19:14:54 +0530

    fix: constants

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { DEFAULT_BANNER_IMAGE } from '../../config/constants';
+import { DEFAULT_BANNER_IMAGE, imagePath } from '../../config/constants';
 import { getRandomNumber, getRoundRobin } from '../../libs/utils/math';
 import Img from './style';
 
@@ -14,31 +14,34 @@ class Slider extends Component {
 
   componentDidMount() {
     const { duration } = this.props;
-    this.timerId = setInterval(() => {
-      this.tick();
+    this.updateImage = setInterval(() => {
+      this.forEachDuration();
     }, duration);
   }
 
   componentWillUnmount() {
-    clearInterval(this.timerId);
+    clearInterval(this.updateImage);
   }
 
-  tick() {
+  forEachDuration() {
     const { random, banners } = this.props;
-    const index = random
+    const { index } = this.state;
+    const currentIndex = random
       ? getRandomNumber(banners.length)
-      : getRoundRobin(this.state.index, banners.length);
+      : getRoundRobin(index, banners.length);
     this.setState({
-      index,
+      index: currentIndex,
     });
   }
 
   render() {
-    const { height, banners } = this.props;
+    const {
+      height, banners, altText, defaultBanner,
+    } = this.props;
     const { index } = this.state;
     return (
       <div>
-        <Img src={banners[index]} alt="" height={height} />
+        <Img src={banners ? `${imagePath}${banners[index]}` : `${imagePath}${defaultBanner}`} alt={altText} height={height} />
       </div>
     );
   }
@@ -47,7 +50,7 @@ class Slider extends Component {
 Slider.propTypes = {
   altText: PropTypes.string,
   defaultBanner: PropTypes.string,
-  banners: PropTypes.array,
+  banners: PropTypes.arrayOf,
   duration: PropTypes.number,
   height: PropTypes.number,
   random: PropTypes.bool,
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,15 +1,10 @@
-export const PUBLIC_IMAGE_FOLDER = '/images/';
-export const DEFAULT_BANNER_IMAGE = 'images/default.png';
-export const DNS_SERVER = 'images/dns-server.png';
-export const LOAD_BALANCER = 'images/load-balancer.png  ';
-export const CLOUD = 'images/cloud.jpg';
-export const JS = 'images/js.jpg';
-export const FULL_STACK = 'images/full-stack-web-development.jpg';
+export const imagePath = '/images/';
+export const DEFAULT_BANNER_IMAGE = 'default.png';
 export const banners = [
-  DEFAULT_BANNER_IMAGE,
-  DNS_SERVER,
-  LOAD_BALANCER,
-  CLOUD,
-  JS,
-  FULL_STACK,
+  'default.png',
+  'dns-server.png',
+  'load-balancer.png',
+  'cloud.jpg',
+  'js.jpg',
+  'full-stack-web-development.jpg',
 ];
