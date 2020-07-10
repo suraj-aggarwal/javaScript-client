98415fdd390a606b62b5e9e583d739400ba876c7
Suraj Aggarwal <suraj.aggarwal@lapr243.successivetech.com>
2020-07-10 18:51:22 +0530

    add: graphQl fetch

---
+++ src/libs/graphql-client.js
@@ -0,0 +1,13 @@
+import { setContext } from 'apollo-link-context';
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token,
+    },
+  };
+});
+
+export default authLink;
